469fde776bb9d15874629c5e5775d74a9f954cc9
Dominik Fijas <dominik@applicake.com>
2010-06-12 15:32:24 +0200

    added Pawlak to candidates feature

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,6 +6,7 @@ class CandidatesController < ApplicationController
   Mikke = {:name => "Janusz Korwin-Mikke", :id => "142395887"}
   Morawiecki = {:name => "Kornel Morawiecki", :id => "138184708"}
   Kaczynski = {:name => "Jarosław Kaczyński", :id => "141919655"}
+  Pawlak = {:name => "Waldemar Pawlak", :id => "143817832"}
   
   def index
     #cache = ActiveSupport::Cache::MemCacheStore.new
@@ -25,14 +26,17 @@ class CandidatesController < ApplicationController
     @twitts_4 = Twitt.find_all_by_username("Jarosław Kaczyński", :order => "date DESC")
     @avatar_4 = Twittpic.find_by_user_id(Kaczynski[:id])
     
-    @twitts_5 = Twitt.find_all_by_username("Janusz Korwin-Mikke", :order => "date DESC")
-    @avatar_5 = Twittpic.find_by_user_id(Mikke[:id])
+    @twitts_5 = Twitt.find_all_by_username("Waldemar Pawlak", :order => "date DESC")
+    @avatar_5 = Twittpic.find_by_user_id(Pawlak[:id])
     
-    @twitts_6 = Twitt.find_all_by_username("Marek Jurek", :order => "date DESC")
-    @avatar_6 = Twittpic.find_by_user_id(Jurek[:id])
+    @twitts_6 = Twitt.find_all_by_username("Janusz Korwin-Mikke", :order => "date DESC")
+    @avatar_6 = Twittpic.find_by_user_id(Mikke[:id])
     
-    @twitts_7 = Twitt.find_all_by_username("Kornel Morawiecki", :order => "date DESC")
-    @avatar_7 = Twittpic.find_by_user_id(Morawiecki[:id])
+    @twitts_7 = Twitt.find_all_by_username("Marek Jurek", :order => "date DESC")
+    @avatar_7 = Twittpic.find_by_user_id(Jurek[:id])
+    
+    @twitts_8 = Twitt.find_all_by_username("Kornel Morawiecki", :order => "date DESC")
+    @avatar_8 = Twittpic.find_by_user_id(Morawiecki[:id])
   end
   
   def get_all_profile_pics
@@ -44,6 +48,7 @@ class CandidatesController < ApplicationController
     get_profile_pic(Mikke[:id])
     get_profile_pic(Morawiecki[:id])
     get_profile_pic(Kaczynski[:id])
+    get_profile_pic(Pawlak[:id])
 
     respond_to do |format|
       format.html {
@@ -62,6 +67,7 @@ class CandidatesController < ApplicationController
     feed(Mikke[:id], Mikke[:name])
     feed(Morawiecki[:id], Morawiecki[:name])
     feed(Kaczynski[:id], Kaczynski[:name])
+    feed(Pawlak[:id], Pawlak[:name])
   end
   
   def feed(id, name)
