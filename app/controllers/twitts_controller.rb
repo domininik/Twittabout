1efced02b153acdcfa40fb7594eacbea028f852f
Dominik Fijas <dominik@applicake.com>
2010-09-06 12:42:02 +0200

    JSON instead of XML in Twitter public_timeline

--- app/controllers/twitts_controller.rb
+++ app/controllers/twitts_controller.rb
@@ -1,5 +1,6 @@
 class TwittsController < ApplicationController
   before_filter :require_user
+
   def index
     respond_to do |format|
       format.html {}
@@ -8,13 +9,13 @@ class TwittsController < ApplicationController
 
   def feed
     @twitts = []
-    url = 'http://api.twitter.com/statuses/public_timeline.xml'
-    xml_data = Net::HTTP.get_response(URI.parse(url)).body
-    doc = REXML::Document.new(xml_data)
-    doc.elements.each('statuses/status') do |ele|
-      @twitts << ele.elements['text'].text
+    url = 'http://api.twitter.com/statuses/public_timeline.json'
+    json = Net::HTTP.get_response(URI.parse(url)).body
+    data = ActiveSupport::JSON.decode(json)
+    data.each do |ele|
+      @twitts << ele['text']
     end
-    
+   
     respond_to do |format|
       format.html {
         render :template => 'twitts/index'
