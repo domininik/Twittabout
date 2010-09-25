a16c2368e20633e433433a920432c7de3eb192c1
Marcin Bunsch <marcin@applicake.com>
2010-09-24 22:06:17 -0500

    Added bundler support, upped Rails version to 2.3.9, removed log

---
+++ Gemfile
@@ -0,0 +1,21 @@
+#!/bin/env ruby
+source :gemcutter
+gem "rails", "~> 2.3.5"
+gem "mysql"
+gem "authlogic"
+gem "will_paginate", "~> 2.3.14"
+gem "mongrel"
+# bundler requires these gems in all environments
+# gem "nokogiri", "1.4.2"
+# gem "geokit"
+
+group :development do
+  # bundler requires these gems in development
+  # gem "rails-footnotes"
+end
+
+group :test do
+  # bundler requires these gems while running tests
+  # gem "rspec"
+  # gem "faker"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,8 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
-  config.gem 'will_paginate', :version => '~> 2.3.14', :source => 'http://gemcutter.org'
+  # config.gem "authlogic"
+  # config.gem 'will_paginate', :version => '~> 2.3.14', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -41,3 +41,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+# Bundler handles the gem, we just require it so it loads up correctly
+require 'will_paginate'
