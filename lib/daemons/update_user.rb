c4d52588a41f8d964b106363f006c9b5e9ac23be
Dominik Fijas <dominik@applicake.com>
2010-10-09 18:36:38 +0200

    added daemon file to update users

---
+++ lib/daemons/update_user.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# You might want to change this
+ENV["RAILS_ENV"] ||= "development"
+
+require File.dirname(__FILE__) + "/../../config/environment.rb"
+
+$running = true
+Signal.trap("TERM") do 
+  $running = false
+end
+
+while $running do
+  TwitterUser.first(:order => "RAND()").update_data
+  sleep 24
+end
